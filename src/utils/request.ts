b08c2b38e8ba8b5392519371be9a1158a2c724c8
David Röger <david@codeatelier.com>
2021-10-10 20:04:52 +0200

    Document request

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,3 +1,9 @@
+/**
+ * Make a request and parse the response
+ * @param input URL
+ * @param init Params
+ * @returns repsonse
+ */
 export async function request(input: RequestInfo, init?: RequestInit) {
     const res = await fetch(input, init);
     console.log(input, init);
