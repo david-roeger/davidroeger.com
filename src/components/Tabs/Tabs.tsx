9a77746f736914ba86e6527724a7a7cda3b68aac
David Röger <david@codeatelier.com>
2021-10-12 21:15:35 +0200

    Add radix tabs

---
+++ src/components/Tabs/Tabs.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import * as Tabs from '@radix-ui/react-tabs';
+
+export const Root = Tabs.Root;
+export const List = Tabs.List;
+export const Trigger = Tabs.Trigger;
+export const Content = Tabs.Content;
