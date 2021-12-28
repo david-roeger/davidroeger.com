c6473cc10922aa5e4dd35be6d2ed5d102151e9e3
David Röger <david@codeatelier.com>
2021-12-28 20:53:45 +0100

    Add section header component

---
+++ sveltekit-test/src/lib/Components/SectionHeader/SectionHeader.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let tag = 'div';
+	let c = '';
+	export { c as class };
+	import { replaceTag } from '$lib/Actions';
+</script>
+
+<div class={`border-b bg-white/60 backdrop-blur-xl ${c}`} use:replaceTag={tag}>
+	<slot />
+</div>
