1813accf4ef400e3f4e49a08bda3a28c6859e96b
David Röger <david@codeatelier.com>
2021-12-28 20:48:25 +0100

    Add mdsvex

--- sveltekit-test/svelte.config.js
+++ sveltekit-test/svelte.config.js
@@ -1,3 +1,5 @@
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
@@ -5,9 +7,11 @@ import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
+
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [preprocess(), mdsvex(mdsvexConfig)],
 
 	kit: {
 		adapter: adapter(),
