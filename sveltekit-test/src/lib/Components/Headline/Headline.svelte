64a44bfd6b1ca143992e99fa2f9254844770924b
David Röger <david@codeatelier.com>
2022-01-03 19:00:13 +0100

    Update headline styling

--- sveltekit-test/src/lib/Components/Headline/Headline.svelte
+++ sveltekit-test/src/lib/Components/Headline/Headline.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
-	export let type: 'primary' | 'secondary' = 'primary';
+	export let type: 'primary' | 'secondary' | 'tertiary' = 'primary';
 	let c = '';
 	export { c as class };
 </script>
 
 <div class={`border-b border-mauve-6 p-2 ${c}`}>
 	{#if type === 'secondary'}
-		<h2 class="text-4xl">
+		<h2 class="text-2xl">
 			<slot />
 		</h2>
+	{:else if type === 'tertiary'}
+		<h3 class="text-1xl">
+			<slot />
+		</h3>
 	{:else}
 		<h1 class="text-6xl">
 			<slot />
