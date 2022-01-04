ebfa6ac0baa1f90d36af9cad6fe3e58cfd723688
David Röger <david@codeatelier.com>
2022-01-05 00:03:51 +0100

    Make headline unstylable

--- sveltekit-test/src/lib/Components/Headline/Headline.svelte
+++ sveltekit-test/src/lib/Components/Headline/Headline.svelte
@@ -1,20 +1,22 @@
 <script lang="ts">
 	export let type: 'primary' | 'secondary' | 'tertiary' = 'primary';
+	export let id: string = '';
+	export let unstyled = false;
 	let c = '';
 	export { c as class };
 </script>
 
-<div class={`border-b border-mauve-6 p-2 ${c}`}>
+<div class={`${unstyled ? '' : 'border-b border-mauve-6 p-2'} ${c}`}>
 	{#if type === 'secondary'}
-		<h2 class="text-2xl">
+		<h2 class={`${unstyled ? '' : 'text-2xl'}`} {id}>
 			<slot />
 		</h2>
 	{:else if type === 'tertiary'}
-		<h3 class="text-xl">
+		<h3 class={`${unstyled ? '' : 'text-xl'}`} {id}>
 			<slot />
 		</h3>
 	{:else}
-		<h1 class="text-6xl">
+		<h1 class={`${unstyled ? '' : 'text-6xl'}`} {id}>
 			<slot />
 		</h1>
 	{/if}
