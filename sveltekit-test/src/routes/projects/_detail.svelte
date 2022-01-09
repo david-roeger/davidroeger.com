c0ffca4fada621229149fc7e359fc2886727e183
David Röger <david@codeatelier.com>
2022-01-09 17:35:14 +0100

    Add mdsvex layout

---
+++ sveltekit-test/src/routes/projects/_detail.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	export let title: string;
+	export let tags: string[];
+	export let thumbnail: string;
+
+	import AccessibleIcon from '$components/AccessibleIcon/AccessibleIcon.svelte';
+	import TagIcon from './assets/icons/24/tag.svg';
+	import Headline from '$components/Headline/Headline.svelte';
+</script>
+
+<section>
+	{#if tags.length}
+		<p class="flex items-center p-1 text-xs bg-white border-b text-mauve-11 border-mauve-6">
+			<AccessibleIcon label="Tags"><TagIcon /></AccessibleIcon>
+			{#each tags as tag (tag)}
+				<span class="m-1">{tag} </span>
+			{/each}
+		</p>
+	{/if}
+	<div class="border-b border-mauve-6">
+		{#if thumbnail}
+			<img
+				height="120"
+				loading="lazy"
+				alt="img"
+				class="block border-r h-44 border-mauve-6"
+				src={`../assets/projects/${thumbnail}`}
+			/>
+		{/if}
+		<Headline class="py-8 border-b-0 md:py-16">{title}</Headline>
+	</div>
+
+	<slot />
+</section>
