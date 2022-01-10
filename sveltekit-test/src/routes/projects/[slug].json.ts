01617ca03ca027daaaf5c42b93e5ed246268827e
David Röger <david@codeatelier.com>
2022-01-10 03:33:47 +0100

    Add type for get

--- sveltekit-test/src/routes/projects/[slug].json.ts
+++ sveltekit-test/src/routes/projects/[slug].json.ts
@@ -1,9 +1,9 @@
 //import { slugFromPath } from '$lib/util';
+import type { GetReturnType } from '$lib/types';
 
-/**
- * @type {import('@sveltejs/kit').RequestHandler}
- */
-export async function get({ params }) {
+/** @type {import('@sveltejs/kit').RequestHandler} */
+
+export async function get({ params }: { params: { slug: string } }): GetReturnType {
 	const modules = import.meta.glob(`./*.{md,svx,svelte.md}`);
 	const { slug } = params;
 	for (const [path, resolver] of Object.entries(modules)) {
