8836824b7cc16ab192c662d954d4b866552bc946
David Röger <david@codeatelier.com>
2022-05-24 11:45:33 +0200

    Add supabase

--- sveltekit-test/src/env.d.ts
+++ sveltekit-test/src/env.d.ts
@@ -4,7 +4,7 @@ interface ImportMetaEnv extends Readonly<Record<string, string>> {
 	readonly VITE_SPOTIFY_REFRESH_TOKEN: string;
 
 	readonly VITE_SUPABASE_URL: string;
-	readonly VITE_SUPABASE_KEY: string;
+	readonly VITE_SUPABASE_ANON_KEY: string;
 }
 
 interface ImportMeta {
