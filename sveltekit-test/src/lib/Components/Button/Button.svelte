d09f9c6976e76b548e2e1fe98105bcd2bf64d285
David Röger <david@codeatelier.com>
2022-05-24 11:45:44 +0200

    Add Button Component

---
+++ sveltekit-test/src/lib/Components/Button/Button.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { buttonType } from '$lib/Actions';
+
+	export let variant: 'primary' | 'icon' | 'rounded' = 'primary';
+	export let type: 'button' | 'submit' | 'reset' = 'button';
+	export let role: string = undefined;
+	export let disabled: boolean = undefined;
+
+	let customClass = '';
+	export { customClass as class };
+
+	const primaryClass = 'px-4 py-2';
+	const roundedClass = 'px-4 py-2 rounded-full';
+	const iconClass = '';
+
+	let disabledClass;
+	if (disabled) {
+		disabledClass =
+			'cursor-not-allowed !bg-white border-mauve-11 ring-mauve-11 text-mauve-11';
+	}
+
+	let variantClass;
+	switch (variant) {
+		case 'primary':
+		default:
+			variantClass = primaryClass;
+			break;
+		case 'icon':
+			variantClass = iconClass;
+			break;
+		case 'rounded':
+			variantClass = roundedClass;
+			break;
+	}
+</script>
+
+<button
+	{disabled}
+	{role}
+	use:buttonType={type}
+	on:click
+	class="border border-mauve-12 focus:outline-none ring-mauve-12 focus:ring-1 transition-colors {variantClass} {customClass} {disabledClass}"
+>
+	<slot />
+</button>
