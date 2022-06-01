7d60cae137a30ab2cb6d2033fa7241cae3578c1d
David Röger <david@codeatelier.com>
2022-06-01 14:16:51 +0200

    Add ids to dreams

--- sveltekit-test/src/routes/experimental/dreams/index.svelte
+++ sveltekit-test/src/routes/experimental/dreams/index.svelte
@@ -39,8 +39,9 @@
 
 <script lang="ts">
 	import { getDreams } from '$lib/Utils/Auth/request';
-	import { session } from '$app/stores';
+	import { page, session } from '$app/stores';
 	import Headline from '$lib/Components/Headline/Headline.svelte';
+	import { user } from '$lib/Utils/Auth/store';
 
 	export let emojis: string[];
 	console.info('experimental/dreams Page: script call');
@@ -61,26 +62,35 @@
 
 <ul class="grid grid-cols-1 p-1 md:grid-cols-2 lg:grid-cols-3">
 	{#each dreams as dream, index (dream.id)}
-		<div>
-			<li
-				class="flex flex-col m-1 border -0 border-mauve-6 bg-white/[.85]"
-			>
-				<div class="flex">
-					<span class="w-10 p-2 text-center border-b border-mauve-6">
+		<li
+			class="flex flex-col m-1 border border-mauve-6 scroll-m-2 {$page.url
+				.hash === `#${dream.id.toString()}`
+				? 'ring-1 ring-mauve-6'
+				: ''}"
+			id={dream.id.toString()}
+		>
+			{#if $user}
+				<p class="bg-white border-b border-mauve-6">edit</p>
+			{/if}
+			<div class="flex bg-white">
+				<span
+					class="w-10 p-2 text-center border-b border-mauve-6 group"
+				>
+					<span class="block group-hover:animate-cool-wiggle">
 						{!!dream.emoji ? dream.emoji : emojis[index]}
 					</span>
-					<Headline type="quaternary" containerClass="grow border-l">
-						{formatDate(dream.created_at)}
-						<span class="text-mauve-11">
-							({formatDate(dream.updated_at)})
-						</span>
-					</Headline>
-				</div>
-				<div class="p-2">
-					<p class="whitespace-pre-line">{dream.text}</p>
-				</div>
-			</li>
-		</div>
+				</span>
+				<Headline type="quaternary" containerClass="grow border-l">
+					{formatDate(dream.created_at)}
+					<span class="text-mauve-11">
+						({formatDate(dream.updated_at)})
+					</span>
+				</Headline>
+			</div>
+			<div class="p-2 bg-white/[.85]">
+				<p class="whitespace-pre-line">{dream.text}</p>
+			</div>
+		</li>
 	{:else}
 		<li>No dreams</li>
 	{/each}
