438899686cc3d0abcf8724729edfe40091edbf2d
David Röger <david@codeatelier.com>
2022-08-25 22:43:21 +0200

    Add layout to experimental

--- sveltekit-new/src/routes/experimental/+layout.svelte
+++ sveltekit-new/src/routes/experimental/+layout.svelte
@@ -7,15 +7,12 @@
 	additionalMetaTags={[
 		{
 			name: 'theme-color',
-			content: '#EDF6FF',
-		},
+			content: '#EDF6FF'
+		}
 	]}
 />
 
-<Background
-	center="hsla(206, 100%, 50.0%, 0.4)"
-	outside="hsla(206, 100%, 50.0%, 0)"
-/>
+<Background center="hsla(206, 100%, 50.0%, 0.4)" outside="hsla(206, 100%, 50.0%, 0)" />
 
 <div class="relative">
 	<slot />
