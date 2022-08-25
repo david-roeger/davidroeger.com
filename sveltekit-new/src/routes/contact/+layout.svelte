dfce8f235e329801e85921c334bd989beeee20de
David Röger <david@codeatelier.com>
2022-08-25 18:16:43 +0200

    Add contatc page

---
+++ sveltekit-new/src/routes/contact/+layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { Background } from '$components/Background';
+	import Head from '$components/Head/Head.svelte';
+</script>
+
+<Head
+	additionalMetaTags={[
+		{
+			name: 'theme-color',
+			content: '#FFF1E7',
+		},
+	]}
+/>
+
+<Background
+	center="hsla(24, 100%, 75.3%, 0.4)"
+	outside="hsla(24, 100%, 75.3%, 0)"
+/>
+
+<div class="relative">
+	<slot />
+</div>
---
+++ sveltekit-new/src/routes/contact/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	console.info('contact Page: script call');
+</script>
+
+<p>contact me</p>
