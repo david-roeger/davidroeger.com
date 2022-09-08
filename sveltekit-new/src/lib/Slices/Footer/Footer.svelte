6f413dde9bfcaacdace95fa016905268d575779e
David Röger <david@codeatelier.com>
2022-09-08 22:30:32 +0200

    Add Footer component

---
+++ sveltekit-new/src/lib/Slices/Footer/Footer.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { browser, prerendering } from '$app/env';
+
+	let c = '';
+	export { c as class };
+
+	console.log(browser);
+	console.log('pre', prerendering);
+</script>
+
+<footer class={`border-t xl:max-w-7xl border-mauve-6 p-2 ${c}`}>
+	<div>
+		<p>Prerendering: {prerendering}</p>
+		<p>Last updated at: {process.env.VITE_BUILD_TIME}</p>
+	</div>
+</footer>
