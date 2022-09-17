84ee06323dad399cb572962f92ff68a030a360ca
David Röger <david@codeatelier.com>
2022-09-17 18:50:05 +0200

    Add form reset and error classes

--- sveltekit-new/src/routes/contact/+page.svelte
+++ sveltekit-new/src/routes/contact/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	console.info('contact: +page.svelte');
 
-	import { enhance } from '$app/forms';
+	import { applyAction, enhance } from '$app/forms';
 	import { page } from '$app/stores';
 	import Button from '$lib/Components/Button/Button.svelte';
 	import type { ActionData } from './$types';
@@ -12,63 +12,112 @@
 	// https://github.com/sveltejs/kit/issues/6823
 	export let form: ActionData;
 
-	$: name = (form?.values?.name ?? '') as string;
-	$: email = (form?.values?.email ?? '') as string;
-	$: tel = (form?.values?.tel ?? '') as string;
-	$: message = (form?.values?.message ?? '') as string;
+	$: name =
+		form?.values?.name && typeof form.values.name === 'string'
+			? form.values.name
+			: '';
+	$: email =
+		form?.values?.email && typeof form.values.email === 'string'
+			? form.values.email
+			: '';
+	$: tel =
+		form?.values?.tel && typeof form.values.tel === 'string'
+			? form.values.tel
+			: '';
+	$: message =
+		form?.values?.message && typeof form.values.message === 'string'
+			? form.values.message
+			: '';
 
 	const getValValidationClass = (
 		value: string | undefined,
 		state: string | undefined,
-		successClass: string,
+		classes: {
+			successClass?: string;
+			errorClass?: string;
+			defaultClass?: string;
+		},
 		required: boolean = false
 	) => {
-		if (!state || state === 'success' || (!value && !required)) return '';
-		if (value) return successClass;
-		return 'to-red-5';
+		if (!state || state === 'success' || (!value && !required))
+			return classes.defaultClass ?? '';
+		if (value) return classes.successClass ?? '';
+		return classes.errorClass ?? '';
+	};
+
+	const focusInvalid = (missing: ActionData['missing']) => {
+		if (missing) {
+			for (const [key, value] of Object.entries(missing)) {
+				if (value) {
+					const element = document.getElementsByName(key)[0];
+					element?.focus();
+					return;
+				}
+			}
+		}
 	};
 </script>
 
 <section class="mb-32">
 	<p>contact me</p>
+	<p>Test</p>
+	<p>Test</p>
+	<p>Test</p>
+	<p>Test</p>
+	<p>Test</p>
+	<p>Test</p>
+	<p>Test</p>
+	<p>Test</p>
+	<p>Test</p>
+	<p>Test</p>
+	<p>Test</p>
+	<p>Test</p>
+	<p>Test</p>
+	<p>Test</p>
+	<p>Test</p>
+	<p>Test</p>
+	<p>Test</p>
+	<p>Test</p>
+	<p>Test</p>
+	<p>Test</p>
+	<p>Test</p>
+	<p>Test</p>
+	<p>Test</p>
+	<p>Test</p>
 	{#if form?.state}<p class="error">{form.state}</p>{/if}
 	{#if form?.id}<p class="error">{form.id}</p>{/if}
+	{#if form?.values}<p class="error">{JSON.stringify(form.values)}</p>{/if}
 
-	<p>Test</p>
-	<p>Test</p>
-	<p>Test</p>
-	<p>Test</p>
-	<p>Test</p>
-	<p>Test</p>
-	<p>Test</p>
-	<p>Test</p>
-	<p>Test</p>
-	<p>Test</p>
-	<p>Test</p>
-	<p>Test</p>
-	<p>Test</p>
-	<p>Test</p>
-	<p>Test</p>
-	<p>Test</p>
-	<p>Test</p>
-	<p>Test</p>
-	<p>Test</p>
-	<p>Test</p>
-	<p>Test</p>
-	<p>Test</p>
-	<p>Test</p>
-	<p>Test</p>
-
+	<p>{JSON.stringify(form?.state, undefined, 2)}</p>
 	<form
 		method="POST"
-		use:enhance
-		class="flex flex-col max-w-xs space-y-4 m-4"
+		use:enhance={({ form }) => {
+			return async ({ result }) => {
+				if (result.type === 'success') {
+					form.reset();
+				}
+				if (result.type === 'invalid') {
+					focusInvalid(result.data?.missing);
+				}
+				await applyAction(result);
+			};
+		}}
+		class="flex flex-col max-w-xs space-y-4 m-4 text-mauve-12"
 	>
 		<input type="hidden" name="url" value={$page.url.toString()} />
 		<div class="flex flex-col items-start group">
 			<label
 				for="name"
-				class="border-mauve-12 rounded-none border border-b-0 text-xs group-focus-within:bg-mauve-12 group-focus-within:text-mauve-1 ring-mauve-12 group-focus-within:ring-1 px-4 py-1"
+				class="border-mauve-12 rounded-none border border-b-0 text-xs ring-mauve-12 group-focus-within:ring-1 px-4 py-1 {getValValidationClass(
+					name,
+					form?.state,
+					{
+						successClass: 'group-focus-within:bg-green-5',
+						errorClass: 'group-focus-within:bg-red-5',
+						defaultClass: 'group-focus-within:bg-orange-5'
+					},
+					true
+				)}"
 			>
 				Name*
 			</label>
@@ -77,7 +126,7 @@
 				class="py-2 px-4 border-mauve-12 rounded-none border w-full group-focus-within:outline-none ring-mauve-12 group-focus-within:ring-1 bg-gradient-to-r from-transparent {getValValidationClass(
 					name,
 					form?.state,
-					'valid:to-green-5',
+					{ successClass: 'to-green-5', errorClass: 'to-red-5' },
 					true
 				)}"
 				name="name"
@@ -92,7 +141,16 @@
 		<div class="flex flex-col items-start group">
 			<label
 				for="email"
-				class="border-mauve-12 rounded-none border border-b-0 text-xs group-focus-within:bg-mauve-12 group-focus-within:text-mauve-1 ring-mauve-12 group-focus-within:ring-1 px-4 py-1"
+				class="border-mauve-12 rounded-none border border-b-0 text-xs ring-mauve-12 group-focus-within:ring-1 px-4 py-1 {getValValidationClass(
+					email,
+					form?.state,
+					{
+						successClass: 'group-focus-within:bg-green-5',
+						errorClass: 'group-focus-within:bg-red-5',
+						defaultClass: 'group-focus-within:bg-orange-5'
+					},
+					true
+				)}"
 			>
 				E-Mail*
 			</label>
@@ -101,7 +159,7 @@
 				class="py-2 px-4 border-mauve-12 rounded-none border w-full group-focus-within:outline-none ring-mauve-12 group-focus-within:ring-1 bg-gradient-to-r from-transparent {getValValidationClass(
 					email,
 					form?.state,
-					'valid:to-green-5',
+					{ successClass: 'to-green-5', errorClass: 'to-red-5' },
 					true
 				)}"
 				name="email"
@@ -116,8 +174,16 @@
 
 		<div class="flex flex-col items-start group">
 			<label
-				for="name"
-				class="border-mauve-12 rounded-none border border-b-0 text-xs group-focus-within:bg-mauve-12 group-focus-within:text-mauve-1 ring-mauve-12 group-focus-within:ring-1 px-4 py-1"
+				for="tel"
+				class="border-mauve-12 rounded-none border border-b-0 text-xs ring-mauve-12 group-focus-within:ring-1 px-4 py-1 {getValValidationClass(
+					tel,
+					form?.state,
+					{
+						successClass: 'group-focus-within:bg-green-5',
+						errorClass: 'group-focus-within:bg-red-5',
+						defaultClass: 'group-focus-within:bg-orange-5'
+					}
+				)}"
 			>
 				Phone
 			</label>
@@ -126,7 +192,7 @@
 				class="py-2 px-4 border-mauve-12 rounded-none border w-full group-focus-within:outline-none ring-mauve-12 group-focus-within:ring-1 bg-gradient-to-r from-transparent {getValValidationClass(
 					tel,
 					form?.state,
-					'valid:to-green-5'
+					{ successClass: 'to-green-5', errorClass: 'to-red-5' }
 				)}"
 				name="tel"
 				autocomplete="tel"
@@ -138,7 +204,16 @@
 		<div class="flex flex-col items-start group">
 			<label
 				for="message"
-				class="border-mauve-12 rounded-none border border-b-0 text-xs group-focus-within:bg-mauve-12 group-focus-within:text-mauve-1 ring-mauve-12 group-focus-within:ring-1 px-4 py-1"
+				class="border-mauve-12 rounded-none border border-b-0 text-xs ring-mauve-12 group-focus-within:ring-1 px-4 py-1 {getValValidationClass(
+					message,
+					form?.state,
+					{
+						successClass: 'group-focus-within:bg-green-5',
+						errorClass: 'group-focus-within:bg-red-5',
+						defaultClass: 'group-focus-within:bg-orange-5'
+					},
+					true
+				)}"
 			>
 				Message*
 			</label>
@@ -149,7 +224,7 @@
 				class="py-2 px-4 border-mauve-12 rounded-none resize-none border w-full group-focus-within:outline-none ring-mauve-12 group-focus-within:ring-1 bg-gradient-to-r from-transparent {getValValidationClass(
 					message,
 					form?.state,
-					'valid:to-green-5',
+					{ successClass: 'to-green-5', errorClass: 'to-red-5' },
 					true
 				)}"
 				value={message}
