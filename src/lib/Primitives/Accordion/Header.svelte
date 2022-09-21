914c3bba61a12b8eea9e2d2f736a310db2ccfb17
David Röger <david@codeatelier.com>
2022-09-21 13:34:25 +0200

    Update accordion header handling

--- src/lib/Primitives/Accordion/Header.svelte
+++ src/lib/Primitives/Accordion/Header.svelte
@@ -2,12 +2,9 @@
 	let c = '';
 	export { c as class };
 
-	/**
-	 * @todo
-	 * add role="heading" handling
-	 */
+	export let as: 'H1' | 'H2' | 'H3' | 'H4' | 'H5' | 'H6' = 'H3';
 </script>
 
-<h3 class={c}>
+<svelte:element this={as} class={c}>
 	<slot />
-</h3>
+</svelte:element>
