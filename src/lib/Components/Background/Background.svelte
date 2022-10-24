d10ca583ca779e707d7823512d0fb944b534196f
David Röger <david@codeatelier.com>
2022-10-24 23:56:33 +0200

    Update background component

--- src/lib/Components/Background/Background.svelte
+++ src/lib/Components/Background/Background.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	export let center: string;
 	export let outside: string;
+
+	export let position: 'absolute' | 'fixed' = 'fixed';
 </script>
 
 <div
-	class="fixed top-0 bottom-0 w-full h-full pointer-events-none xl:max-w-7xl -z-10"
+	class="{position} top-0 bottom-0 w-full h-full pointer-events-none xl:max-w-7xl -z-10"
 	style="background: radial-gradient(50% 50% at 50% 50%, {center}, {outside} 100%);"
 />
