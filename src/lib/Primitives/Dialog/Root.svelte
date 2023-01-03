df3ecb62ed746e3786d4df45934dcbdb754b3932
David Röger <david@codeatelier.com>
2023-01-03 23:46:40 +0100

    Fix scroll body lock

--- src/lib/Primitives/Dialog/Root.svelte
+++ src/lib/Primitives/Dialog/Root.svelte
@@ -9,7 +9,7 @@
 	export { c as class };
 
 	import { activeDialogs } from './store';
-	import { onDestroy, setContext } from 'svelte';
+	import { onDestroy, onMount, setContext } from 'svelte';
 	import { writable } from 'svelte/store';
 	import { createEventDispatcher } from 'svelte';
 	import type { RootContext } from './types';
@@ -29,10 +29,21 @@
 
 	$setOpen = () => {
 		$activeDialogs = [...$activeDialogs, computedId];
-		document.body.classList.add('dialog-open');
+		if ($activeDialogs.length === 1) {
+			const topOffset = document.documentElement.scrollTop;
+			document.body.style.setProperty('--topOffset', -topOffset + 'px');
+			document.body.classList.add('dialog-open');
+		}
 
 		$open = true;
 	};
+
+	onMount(() => {
+		if (defaultOpen && $setOpen) {
+			$setOpen();
+		}
+	});
+
 	$setClose = () => {
 		if ($trap) {
 			$trap.deactivate();
@@ -47,6 +58,14 @@
 
 		if ($activeDialogs.length === 0) {
 			document.body.classList.remove('dialog-open');
+			const prevOffset =
+				document.body.style.getPropertyValue('--topOffset');
+			console.log(prevOffset);
+			window.scrollTo({
+				top: -parseInt(
+					document.body.style.getPropertyValue('--topOffset')
+				)
+			});
 		}
 		$open = false;
 	};
