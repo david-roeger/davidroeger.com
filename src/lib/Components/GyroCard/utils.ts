482690ddf2af6f1acfbae9e3e5b19637473729ad
David Röger <david@codeatelier.com>
2023-01-05 01:51:31 +0100

    Add logging of DeviceOrientationEvent

--- src/lib/Components/GyroCard/utils.ts
+++ src/lib/Components/GyroCard/utils.ts
@@ -1,4 +1,14 @@
 export const requestDeviceOrientationPermission = async () => {
+	console.log('requestDeviceOrientationPermission');
+	console.log(
+		'typeof DeviceOrientationEvent PermissionState',
+		typeof DeviceOrientationEvent.requestPermission
+	);
+	console.log('DeviceOrientationEvent', DeviceOrientationEvent);
+	console.log(
+		'DeviceOrientationEvent.requestPermission',
+		DeviceOrientationEvent.requestPermission
+	);
 	if (typeof DeviceMotionEvent.requestPermission === 'function') {
 		const response = await DeviceMotionEvent.requestPermission();
 		if (response == 'granted') {
