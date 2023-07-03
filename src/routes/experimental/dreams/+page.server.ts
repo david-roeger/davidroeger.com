b404d2b15262fa5b397e8f30144c1c12a6ae957b
David Röger <david@codeatelier.com>
2023-07-03 23:34:03 +0200

    Add render logs

--- src/routes/experimental/dreams/+page.server.ts
+++ src/routes/experimental/dreams/+page.server.ts
@@ -1,8 +1,12 @@
+console.info('experimental/dreams: +page.server.ts');
+
 import type { PageServerLoad } from './$types';
 
 import client from '$lib/Utils/Supabase/client';
 
 export const load: PageServerLoad = async () => {
+	console.info('experimental/dreams: +page.server.ts // load');
+
 	const dreams = await client.query('SELECT * from dreams');
 	return {
 		dreams: dreams.rows
