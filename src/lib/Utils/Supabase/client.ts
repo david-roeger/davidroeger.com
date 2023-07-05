b901fbffb7033d1c977960ab0c19a03f8f0d0935
David Röger <david@codeatelier.com>
2023-07-05 18:19:15 +0200

    log host

--- src/lib/Utils/Supabase/client.ts
+++ src/lib/Utils/Supabase/client.ts
@@ -1,13 +1,14 @@
 import pg from 'pg';
 import { env } from '$env/dynamic/private';
 
+console.log('@@@ host', env.SUPABASE_HOST);
 const client = new pg.Client({
-    host: env.SUPABASE_HOST,
-    user: env.SUPABASE_USER,
-    password: env.SUPABASE_PASSWORD,
-    database: env.SUPABASE_DATABASE,
-    port: parseInt(env.SUPABASE_PORT),
-  });
+	host: env.SUPABASE_HOST,
+	user: env.SUPABASE_USER,
+	password: env.SUPABASE_PASSWORD,
+	database: env.SUPABASE_DATABASE,
+	port: parseInt(env.SUPABASE_PORT)
+});
 
 await client.connect();
 
