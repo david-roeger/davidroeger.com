34c30f7be8048da6297bc43605833c5fb5cda4e1
David Röger <david@codeatelier.com>
2023-07-05 20:08:24 +0200

    Add env log

--- src/routes/experimental/dreams/+page.server.ts
+++ src/routes/experimental/dreams/+page.server.ts
@@ -2,6 +2,12 @@ console.info('experimental/dreams: +page.server.ts');
 
 import type { PageServerLoad } from './$types';
 
+import { env } from '$env/dynamic/private';
+import { SUPABASE_HOST } from '$env/static/private';
+
+console.log('@1@ host', env.SUPABASE_HOST);
+console.log('@2@ host', SUPABASE_HOST);
+
 // import client from '$lib/Utils/Supabase/client';
 
 export const load: PageServerLoad = async () => {
