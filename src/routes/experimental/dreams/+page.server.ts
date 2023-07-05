8d24585dfceb72202f3424f7f1f9d21aab8322f0
David Röger <david@codeatelier.com>
2023-07-05 20:32:46 +0200

    More logs

--- src/routes/experimental/dreams/+page.server.ts
+++ src/routes/experimental/dreams/+page.server.ts
@@ -3,10 +3,11 @@ console.info('experimental/dreams: +page.server.ts');
 import type { PageServerLoad } from './$types';
 
 import { env } from '$env/dynamic/private';
-import { SUPABASE_HOST } from '$env/static/private';
+import * as a from '$env/static/private';
 
-console.log('@1@ host', env.SUPABASE_HOST);
-console.log('@2@ host', SUPABASE_HOST);
+console.log('@1@', env.SUPABASE_HOST);
+console.log('@2@', a.SUPABASE_HOST);
+console.log('@3@', process.env.SUPABASE_HOST);
 
 // import client from '$lib/Utils/Supabase/client';
 
