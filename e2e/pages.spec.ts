cc3a1273fb6504d6d505af22f62699a524b4fde2
David Röger <david@codeatelier.com>
2023-09-28 22:00:07 +0200

    Fix e2e tests

--- e2e/pages.spec.ts
+++ e2e/pages.spec.ts
@@ -3,15 +3,18 @@ import {} from 'node:test';
 
 test('index page has expected h1', async ({ page }) => {
 	await page.goto('/');
-	expect(await page.textContent('h1')).toBe('Welcome');
+	expect(await page.textContent('h1')).toBe(
+		'  D  A  V  I  D  ::  R  O  E  G  E  R             '
+	);
 });
 
 test('project page has expected h1', async ({ page }) => {
 	await page.goto('/projects');
-	expect(await page.textContent('h1')).toBe('My Projects');
+	console.log(await page.textContent('h1'));
+	expect(await page.textContent('h1')).toBe('::\n\t\tProjects');
 });
 
 test('project detail page has expected h1', async ({ page }) => {
-	await page.goto('/projects/obskurra');
-	expect(await page.textContent('h1')).toBe('Obskurra Typeface');
+	await page.goto('/projects/obskurra-typeface');
+	expect(await page.textContent('h1')).toBe(':: Obskurra Typeface');
 });
