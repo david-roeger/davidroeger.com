011a19f85b4ddd286946fb014d69cb95c046442e
David Röger <david@codeatelier.com>
2023-12-08 22:00:35 +0100

    Use icon for cube

--- src/lib/Components/Cube/Cube.svelte
+++ src/lib/Components/Cube/Cube.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import East from '$assets/Icons/24/east.svg?component';
 	import type { Colors } from '$utils/colors';
 	import { CUBE_COLOR_CLASSES } from './constants';
 
@@ -11,58 +12,14 @@
 			variant
 		].arrow}"
 	>
-		<svg
-			class="stacked"
-			width="24"
-			height="24"
-			viewBox="0 0 24 24"
-			fill="none"
-			xmlns="http://www.w3.org/2000/svg"
-		>
-			<rect
-				x="4"
-				y="11.5"
-				width="15"
-				height="1"
-				rx="0.5"
-				fill="currentColor"
-			/>
-			<path
-				fill-rule="evenodd"
-				clip-rule="evenodd"
-				d="M19.3137 12.7176L20.0208 12.0105L20.0208 12.0104L19.3137 11.3033L19.3137 11.3034L14.3639 6.35361C14.1686 6.15835 13.8521 6.15835 13.6568 6.35361C13.4615 6.54887 13.4615 6.86545 13.6568 7.06071L18.6065 12.0105L13.6569 16.9602C13.4616 17.1554 13.4616 17.472 13.6569 17.6673C13.8521 17.8625 14.1687 17.8625 14.364 17.6673L19.3137 12.7176Z"
-				fill="currentColor"
-			/>
-		</svg>
+		<East />
 	</span>
 	<span
 		class="face side {CUBE_COLOR_CLASSES[variant].side} {CUBE_COLOR_CLASSES[
 			variant
 		].arrow}"
 	>
-		<svg
-			class="stacked {CUBE_COLOR_CLASSES[variant].arrow}"
-			width="24"
-			height="24"
-			viewBox="0 0 24 24"
-			fill="none"
-			xmlns="http://www.w3.org/2000/svg"
-		>
-			<rect
-				x="4"
-				y="11.5"
-				width="15"
-				height="1"
-				rx="0.5"
-				fill="currentColor"
-			/>
-			<path
-				fill-rule="evenodd"
-				clip-rule="evenodd"
-				d="M19.3137 12.7176L20.0208 12.0105L20.0208 12.0104L19.3137 11.3033L19.3137 11.3034L14.3639 6.35361C14.1686 6.15835 13.8521 6.15835 13.6568 6.35361C13.4615 6.54887 13.4615 6.86545 13.6568 7.06071L18.6065 12.0105L13.6569 16.9602C13.4616 17.1554 13.4616 17.472 13.6569 17.6673C13.8521 17.8625 14.1687 17.8625 14.364 17.6673L19.3137 12.7176Z"
-				fill="currentColor"
-			/>
-		</svg>
+		<East />
 	</span>
 </span>
 
@@ -101,7 +58,7 @@
 		width: 100%;
 	}
 
-	.face svg {
+	.face :global(svg) {
 		width: 100%;
 		height: 100%;
 	}
